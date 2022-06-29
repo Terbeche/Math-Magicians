44726d2434fcad109210e58670db0d7e4e79c58c
Terbeche Mustapha <termus96@gmail.com>
2022-06-29 12:36:06 +0100

    add basic calculator UI

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import Calculator from './components/Calculator';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello</h1>
+        <Calculator />
       </header>
     </div>
   );
